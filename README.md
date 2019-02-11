71f2bbbd62d2bccf8f00a2e48004fc2f9dddbce4
Marian Vanderka <vanderka.marian@gmail.com>
2019-02-11 15:41:42 +0100

    release & readme update

--- README.md
+++ README.md
@@ -4,4 +4,10 @@
 ### setup
 
 - follow burner wallet dev setup
-- connect mobile through usb and call `adb reverse tcp:8545 tcp:8545`
+- connect device through usb/emulator and call `adb reverse tcp:8545 tcp:8545` to bridge to localhost
+- shake device to open 'Debug Drawer' or call `adb shell am start com.vander.burner.debug/com.vander.burner.app.ui.DebugActivity`
+
+debug drawer provides endpoints for:
+- `http://localhost:8545`
+- `https://dai.poa.network`
+
--- app/src/release/java/com/vander/burner/app/ReleaseAppModule.kt
+++ app/src/release/java/com/vander/burner/app/ReleaseAppModule.kt
@@ -1,8 +1,11 @@
 package com.vander.burner.app
 
+import com.vander.burner.app.di.Xdai
 import com.vander.scaffold.ui.ViewContainer
 import dagger.Module
 import dagger.Provides
+import dagger.Reusable
+import okhttp3.HttpUrl
 import okhttp3.OkHttpClient
 
 @Module(includes = [(App.Module::class)])
@@ -11,5 +14,8 @@ object ReleaseAppModule {
   @JvmStatic @Provides fun providesViewContainer() = ViewContainer.DEFAULT
 
   @JvmStatic @Provides fun providesOkHttpBuilder(okHttp: OkHttpClient.Builder): OkHttpClient = okHttp.build()
+
+  @JvmStatic @Provides @Reusable @Xdai
+  fun providesXDaiProviderUrl(): HttpUrl = HttpUrl.parse("https://dai.poa.network")!!
 
 }
